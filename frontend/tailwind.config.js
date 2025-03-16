3b017d3004dd39d96d46ec082c309e22a4d4cc5c
ss-engineer-CTG <a0348@g.maizuru-ct.ac.jp>
2025-03-16 22:19:22 +0900

    first-commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './frontend/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
